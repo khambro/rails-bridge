31609c405e9593bfe1e8f2daf235ed20a8c9e071
Karen Hambro <khambro@gmail.com>
2015-10-06 15:34:22 -0700

    commits job board project

---
+++ job_board/db/migrate/20151006174923_create_jobs.rb
@@ -0,0 +1,10 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+      t.text :title
+      t.text :description
+      t.text :salary
+      t.text :startdate
+      t.timestamps null: false
+  end
+  end
